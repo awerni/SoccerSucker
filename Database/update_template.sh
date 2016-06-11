8924ffe9f82d6fe52c1e91e094bf9d3374e0b9df
Andreas Wernitznig <andreas@wernitznig.com>
2016-06-11 22:55:06 +0200

    new

---
+++ Database/update_template.sh
@@ -0,0 +1,9 @@
+#!/bin/sh
+
+cd /my/DB/dir/database 
+
+alias d='psql EuroCup2016 postgres -h dbserver'
+
+echo "DROP TRIGGER checkTip ON tip;" | d 
+cat update.sql | d 
+echo "CREATE TRIGGER checkTip BEFORE INSERT OR UPDATE ON tip FOR EACH ROW EXECUTE PROCEDURE checkTip();" | d
