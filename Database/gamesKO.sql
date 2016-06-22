f90e78470c2262944d6fb6699c95d259ada68946
Andreas Wernitznig <andreas@wernitznig.com>
2016-06-22 22:30:22 +0200

    new ko games

--- Database/gamesKO.sql
+++ Database/gamesKO.sql
@@ -1,4 +1,5 @@
---INSERT INTO game (gameid, team1, team2, city, starttime, kogame) VALUES (91, 'France', 'Romania', 'Saint-Denis', 'Jun 25, 2016 21:00', TRUE);
---INSERT INTO game (gameid, team1, team2, city, starttime, kogame) VALUES (92, 'Austria', 'Italy', 'Saint-Denis', 'Jun 25, 2016 21:00', TRUE);
 INSERT INTO game (gameid, team1, team2, city, starttime, kogame) VALUES (37, 'Switzerland', 'Poland', 'Saint-Étienne', 'Jun 25, 2016 15:00', TRUE);
+INSERT INTO game (gameid, team1, team2, city, starttime, kogame) VALUES (39, 'Croatia', 'Portugal', 'Lens', 'Jun 25, 2016 21:00', TRUE);
+INSERT INTO game (gameid, team1, team2, city, starttime, kogame) VALUES (41, 'Germany', 'Slovakia', 'Villeneuve-d'Ascq', 'Jun 26, 2016 18:00', TRUE);
 INSERT INTO game (gameid, team1, team2, city, starttime, kogame) VALUES (43, 'Italy', 'Spain', 'Saint-Denis', 'Jun 27, 2016 18:00', TRUE);
+INSERT INTO game (gameid, team1, team2, city, starttime, kogame) VALUES (44, 'England', 'Iceland', 'Nice', 'Jun 27, 2016 21:00', TRUE);
