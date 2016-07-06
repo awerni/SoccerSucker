9b73241ebd7a07e94d465f47a3859f91b587c319
Andreas Wernitznig <andreas@wernitznig.com>
2016-07-06 22:45:48 +0200

    added game 49

--- Database/update.sql
+++ Database/update.sql
@@ -35,8 +35,7 @@
 --update game set halftimegoals1 = 0, halftimegoals2 = 0, regulartimegoals1 = 0, regulartimegoals2 = 1 WHERE gameid = 35;
 --update game set halftimegoals1 = 0, halftimegoals2 = 0, regulartimegoals1 = 0, regulartimegoals2 = 1 WHERE gameid = 36;
 
---update game set halftimegoals1= 0, halftimegoals2 = 1, regulartimegoals1 = 1, regulartimegoals2 = 1, 
---                overtimegoals1 = 1, overtimegoals2 = 1, penaltygoals1 = 4, penaltygoals2 = 5 WHERE gameid = 37;
+--update game set halftimegoals1= 0, halftimegoals2 = 1, regulartimegoals1 = 1, regulartimegoals2 = 1, overtimegoals1 = 1, overtimegoals2 = 1, penaltygoals1 = 4, penaltygoals2 = 5 WHERE gameid = 37;
 
 --update game set halftimegoals1 = 0, halftimegoals2 = 0, regulartimegoals1 = 1, regulartimegoals2 = 0 WHERE gameid = 38;
 
@@ -54,4 +53,5 @@
 
 --update game set halftimegoals1 = 1, halftimegoals2 = 0, regulartimegoals1 = 1, regulartimegoals2 = 1, overtimegoals1 = 1, overtimegoals2 = 1, penaltygoals1 = 6, penaltygoals2 = 5 where gameid = 47;
 
-update game set halftimegoals1 = 4, halftimegoals2 = 0, regulartimegoals1 = 5, regulartimegoals2 = 2 WHERE gameid = 48;
+--update game set halftimegoals1 = 4, halftimegoals2 = 0, regulartimegoals1 = 5, regulartimegoals2 = 2 WHERE gameid = 48;
+update game set halftimegoals1 = 0, halftimegoals2 = 0, regulartimegoals1 = 2, regulartimegoals2 = 0 WHERE gameid = 49;
