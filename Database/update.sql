4d171cf45a7fdb1dcdc459b2b714ca2f16a81ced
Andreas Wernitznig <andreas@wernitznig.com>
2016-07-10 23:30:42 +0200

    final game

--- Database/update.sql
+++ Database/update.sql
@@ -55,4 +55,5 @@
 
 --update game set halftimegoals1 = 4, halftimegoals2 = 0, regulartimegoals1 = 5, regulartimegoals2 = 2 WHERE gameid = 48;
 --update game set halftimegoals1 = 0, halftimegoals2 = 0, regulartimegoals1 = 2, regulartimegoals2 = 0 WHERE gameid = 49;
-update game set halftimegoals1 = 0, halftimegoals2 = 1, regulartimegoals1 = 0, regulartimegoals2 = 2 WHERE gameid = 50;
+--update game set halftimegoals1 = 0, halftimegoals2 = 1, regulartimegoals1 = 0, regulartimegoals2 = 2 WHERE gameid = 50;
+update game set halftimegoals1 = 0, halftimegoals2 = 0, regulartimegoals1 = 0, regulartimegoals2 = 0, overtimegoals1 = 1, overtimegoals2 = 0 WHERE gameid = 51;
