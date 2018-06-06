2ffe6e72576a361d4a272d1fafbecc6b3d2495d7
Andreas Wernitznig <andreas@wernitznig.com>
2018-06-07 01:23:20 +0200

    inital import

---
+++ Database/artificialPlayers.sql
@@ -0,0 +1,5 @@
+INSERT INTO player (username, name, firstname, nationality, artificial) values ('average', 'Average', 'Mister', 'Any', TRUE);
+INSERT INTO player (username, name, firstname, nationality, artificial, expertstatus) values ('expert', 'Expert', 'Lady', 'Any', TRUE, 3);
+INSERT INTO player (username, name, firstname, nationality, artificial, expertstatus) values ('intermediate', 'Intermediate', 'Sir', 'Any', TRUE, 2);
+INSERT INTO player (username, name, firstname, nationality, artificial, expertstatus) values ('beginner', 'Beginner', 'Bloddy', 'Any', TRUE, 1);
+INSERT INTO player (username, name, firstname, nationality, artificial) values ('rational', 'Ranking', 'Fifa', 'Any', TRUE);
