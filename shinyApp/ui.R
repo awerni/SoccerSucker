ddacf6f01169ff3c3ef2eece73e699ec2dddd6c1
Andreas Wernitznig <andreas@wernitznig.com>
2018-06-10 01:27:39 +0200

    introduced human players and bots

--- shinyApp/ui.R
+++ shinyApp/ui.R
@@ -5,18 +5,19 @@ library(shinythemes)
 source("settings.R")
 source("function.R")
 
-shinyUI(fluidPage(theme = shinytheme("spacelab"),
-  
+shinyUI(fluidPage(theme = shinytheme("cerulean"),
   titlePanel(trans("sportsevent"), windowTitle = "WC 2018"),
   tags$head(tags$script(src = "message-handler.js")),
   fluidRow(
     column(2,
            img(src='GoStrong.png', width="200", align = "center"),
            hr(),
-           actionButton("refresh", label = trans("refresh")),
+           radioButtons("showplayers", "show:", c("humans" = "human", "humans + bots" = "human_bot", "bots" = "bot")),
            hr(),
            h3(textOutput("user")),
-           uiOutput("status")
+           uiOutput("status"),
+           br(),
+           actionButton("refresh", label = trans("refresh"))
     ),
     column(10,
            tabsetPanel(
@@ -24,9 +25,10 @@ shinyUI(fluidPage(theme = shinytheme("spacelab"),
              tabPanel(trans("placebets"), uiOutput("placebets")),
              tabPanel(trans("checkyourresults"), uiOutput("yourresults")),
              tabPanel(trans("graph"), tabsetPanel(
-               tabPanel(trans("heatmap"), d3heatmapOutput("heatmap")),
+               tabPanel(trans("heatmap"), plotOutput("heatmap")),
                tabPanel(trans("lineranking"), uiOutput("rankingTab")),
                tabPanel(trans("nationality"), uiOutput("nationplot")),
+               tabPanel(trans("expertstatus"), uiOutput("expertplot")),
                tabPanel(trans("pcapoints"), uiOutput("pcaPoints")),
                tabPanel(trans("pcatips"), uiOutput("pcaTips")),
                tabPanel(trans("betstatistics"), plotOutput("betstat", width = "100%", height = "400px"), textOutput("betstatdesc")),
