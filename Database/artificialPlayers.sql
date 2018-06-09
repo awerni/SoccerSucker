63e164bed8a83ef1aff78caa098d90defbc2ae7a
Andreas Wernitznig <andreas@wernitznig.com>
2018-06-10 01:29:01 +0200

    introduced bots

--- Database/artificialPlayers.sql
+++ Database/artificialPlayers.sql
@@ -2,4 +2,4 @@ INSERT INTO player (username, name, firstname, nationality, artificial) values (
 INSERT INTO player (username, name, firstname, nationality, artificial, expertstatus) values ('expert', 'Expert', 'Lady', 'Any', TRUE, 3);
 INSERT INTO player (username, name, firstname, nationality, artificial, expertstatus) values ('intermediate', 'Intermediate', 'Sir', 'Any', TRUE, 2);
 INSERT INTO player (username, name, firstname, nationality, artificial, expertstatus) values ('beginner', 'Beginner', 'Bloddy', 'Any', TRUE, 1);
-INSERT INTO player (username, name, firstname, nationality, artificial) values ('rational', 'Ranking', 'Fifa', 'Any', TRUE);
+INSERT INTO player (username, name, firstname, nationality, artificial) values ('rational', 'Fifa', 'Fiffi', 'Any', TRUE);
