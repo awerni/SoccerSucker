e4700db3250655969117cf1b68f71a07a1a187c0
Andreas Wernitznig <andreas@wernitznig.com>
2018-06-14 22:37:01 +0200

    switched checkLogin() to differnt table

--- Database/storedproceduresPassword_plpgsql.sql
+++ Database/storedproceduresPassword_plpgsql.sql
@@ -2,7 +2,7 @@ CREATE OR REPLACE FUNCTION checkLogin(user1 TEXT, pass TEXT) RETURNS INT2 AS $$
 DECLARE
   ret BOOL;
 BEGIN
-  SELECT INTO ret p.pass = md5($2) FROM player p WHERE username = $1;
+  SELECT INTO ret g.password = md5($2) FROM gameuser g WHERE username = $1;
   IF ret THEN
     RETURN 1;
   END IF;
