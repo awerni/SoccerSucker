a45deeb982aacf3aeb31bcde2b799882134caae0
Andreas Wernitznig <andreas@wernitznig.com>
2018-06-16 14:17:08 +0200

    added game 5

--- Database/update.sql
+++ Database/update.sql
@@ -2,6 +2,7 @@ update game set halftimegoals1 = 2, halftimegoals2 = 0, regulartimegoals1 = 5, r
 update game set halftimegoals1 = 0, halftimegoals2 = 0, regulartimegoals1 = 0, regulartimegoals2 = 1 WHERE gameid = 2;
 update game set halftimegoals1 = 2, halftimegoals2 = 1, regulartimegoals1 = 3, regulartimegoals2 = 3 WHERE gameid = 3;
 update game set halftimegoals1 = 0, halftimegoals2 = 0, regulartimegoals1 = 0, regulartimegoals2 = 1 WHERE gameid = 4;
+update game set halftimegoals1 = 0, halftimegoals2 = 0, regulartimegoals1 = 2, regulartimegoals2 = 1 WHERE gameid = 5;
 
 --update game set halftimegoals1 = 0, halftimegoals2 = 1, regulartimegoals1 = 0, regulartimegoals2 = 2 WHERE gameid = 50;
 --update game set halftimegoals1 = 0, halftimegoals2 = 0, regulartimegoals1 = 0, regulartimegoals2 = 0, overtimegoals1 = 1, overtimegoals2 = 0 WHERE gameid = 51;
