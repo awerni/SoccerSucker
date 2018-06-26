54b7c2e142a9dcf3cce506549a0c392568ba1942
wernitzn <andreas.wernitznig@boehringer-ingelheim.com>
2018-06-26 17:55:09 +0200

    added game 37+38

--- Database/update.sql
+++ Database/update.sql
@@ -34,7 +34,8 @@ update game set halftimegoals1 = 2, halftimegoals2 = 0, regulartimegoals1 = 3, r
 update game set halftimegoals1 = 1, halftimegoals2 = 1, regulartimegoals1 = 2, regulartimegoals2 = 1 WHERE gameid = 34;
 update game set halftimegoals1 = 0, halftimegoals2 = 1, regulartimegoals1 = 1, regulartimegoals2 = 1 WHERE gameid = 35;
 update game set halftimegoals1 = 1, halftimegoals2 = 1, regulartimegoals1 = 2, regulartimegoals2 = 2 WHERE gameid = 36;
-
+update game set halftimegoals1 = 0, halftimegoals2 = 0, regulartimegoals1 = 0, regulartimegoals2 = 0 WHERE gameid = 37;
+update game set halftimegoals1 = 0, halftimegoals2 = 1, regulartimegoals1 = 0, regulartimegoals2 = 2 WHERE gameid = 38;
 
 --update game set halftimegoals1 = 0, halftimegoals2 = 1, regulartimegoals1 = 0, regulartimegoals2 = 2 WHERE gameid = 50;
 --update game set halftimegoals1 = 0, halftimegoals2 = 0, regulartimegoals1 = 0, regulartimegoals2 = 0, overtimegoals1 = 1, overtimegoals2 = 0 WHERE gameid = 51;
