b42dac1fffbafeced1e0cbded7f994f6df0dd0a3
Andreas Wernitznig <andreas@wernitznig.com>
2018-07-02 21:58:57 +0200

    added game 58 and result 54

--- Database/update.sql
+++ Database/update.sql
@@ -53,7 +53,9 @@ update game set halftimegoals1 = 1, halftimegoals2 = 1, regulartimegoals1 = 1, r
 update game set halftimegoals1 = 1, halftimegoals2 = 1, regulartimegoals1 = 1, regulartimegoals2 = 1, overtimegoals1 = 1, overtimegoals2 = 1, penaltygoals1 = 3, penaltygoals2 = 2 WHERE gameid = 52;
 
 update game set halftimegoals1 = 0, halftimegoals2 = 0, regulartimegoals1 = 2, regulartimegoals2 = 0 WHERE gameid = 53;
+update game set halftimegoals1 = 0, halftimegoals2 = 0, regulartimegoals1 = 3, regulartimegoals2 = 2 WHERE gameid = 54;
 
 --update game set halftimegoals1 = 0, halftimegoals2 = 1, regulartimegoals1 = 0, regulartimegoals2 = 2 WHERE gameid = 50;
 --update game set halftimegoals1 = 0, halftimegoals2 = 0, regulartimegoals1 = 0, regulartimegoals2 = 0, overtimegoals1 = 1, overtimegoals2 = 0 WHERE gameid = 51;
 
+INSERT INTO game (gameid, team1, team2, city, starttime, kogame) VALUES (58, 'Brazil', 'Belgium', 'Kazan', 'Jul 6, 2018 21:00+3', TRUE);
