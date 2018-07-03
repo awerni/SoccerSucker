581a156eb0ac2deec5f44b57bd6601ceaaf55ab7
wernitzn <andreas.wernitznig@boehringer-ingelheim.com>
2018-07-03 17:51:59 +0200

    added game 55

--- Database/update.sql
+++ Database/update.sql
@@ -54,8 +54,7 @@ update game set halftimegoals1 = 1, halftimegoals2 = 1, regulartimegoals1 = 1, r
 
 update game set halftimegoals1 = 0, halftimegoals2 = 0, regulartimegoals1 = 2, regulartimegoals2 = 0 WHERE gameid = 53;
 update game set halftimegoals1 = 0, halftimegoals2 = 0, regulartimegoals1 = 3, regulartimegoals2 = 2 WHERE gameid = 54;
+update game set halftimegoals1 = 0, halftimegoals2 = 0, regulartimegoals1 = 1, regulartimegoals2 = 0 WHERE gameid = 55;
 
 --update game set halftimegoals1 = 0, halftimegoals2 = 1, regulartimegoals1 = 0, regulartimegoals2 = 2 WHERE gameid = 50;
 --update game set halftimegoals1 = 0, halftimegoals2 = 0, regulartimegoals1 = 0, regulartimegoals2 = 0, overtimegoals1 = 1, overtimegoals2 = 0 WHERE gameid = 51;
-
-INSERT INTO game (gameid, team1, team2, city, starttime, kogame) VALUES (58, 'Brazil', 'Belgium', 'Kazan', 'Jul 6, 2018 21:00+3', TRUE);
