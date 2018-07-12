ce7e9e7bc8a18958c0769fce3a1b460b1467d99a
wernitzn <andreas.wernitznig@boehringer-ingelheim.com>
2018-07-12 17:13:04 +0200

    removed game inserts, commented out updated games

--- Database/update.sql
+++ Database/update.sql
@@ -61,7 +61,5 @@
 --update game set halftimegoals1 = 1, halftimegoals2 = 1, regulartimegoals1 = 1, regulartimegoals2 = 1, overtimegoals1 = 2, overtimegoals2 = 2, penaltygoals1 = 3, penaltygoals2 = 4 WHERE gameid = 59;
 --update game set halftimegoals1 = 0, halftimegoals2 = 1, regulartimegoals1 = 0, regulartimegoals2 = 2 WHERE gameid = 60;
 
-update game set halftimegoals1 = 0, halftimegoals2 = 0, regulartimegoals1 = 1, regulartimegoals2 = 0 WHERE gameid = 61;
-update game set halftimegoals1 = 0, halftimegoals2 = 1, regulartimegoals1 = 1, regulartimegoals2 = 1, overtimegoals1 = 2, overtimegoals2 = 1 WHERE gameid = 62;
-INSERT INTO game (gameid, team1, team2, city, starttime, kogame) VALUES (63, 'Belgium', 'England', 'Saint Petersburg', 'Jul 14, 2018 17:00+3', TRUE);
-INSERT INTO game (gameid, team1, team2, city, starttime, kogame) VALUES (64, 'France', 'Croatia', 'Moscow', 'Jul 15, 2018 18:00+3', TRUE);
+--update game set halftimegoals1 = 0, halftimegoals2 = 0, regulartimegoals1 = 1, regulartimegoals2 = 0 WHERE gameid = 61;
+--update game set halftimegoals1 = 0, halftimegoals2 = 1, regulartimegoals1 = 1, regulartimegoals2 = 1, overtimegoals1 = 2, overtimegoals2 = 1 WHERE gameid = 62;
