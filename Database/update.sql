cd758c88fe49d878fa15cb648031ea738dd80a99
Andreas Wernitznig <andreas@wernitznig.com>
2018-07-14 17:47:14 +0200

    added game 63

--- Database/update.sql
+++ Database/update.sql
@@ -63,3 +63,5 @@
 
 --update game set halftimegoals1 = 0, halftimegoals2 = 0, regulartimegoals1 = 1, regulartimegoals2 = 0 WHERE gameid = 61;
 --update game set halftimegoals1 = 0, halftimegoals2 = 1, regulartimegoals1 = 1, regulartimegoals2 = 1, overtimegoals1 = 2, overtimegoals2 = 1 WHERE gameid = 62;
+
+update game set halftimegoals1 = 1, halftimegoals2 = 0, regulartimegoals1 = 2, regulartimegoals2 = 0 WHERE gameid = 63;
