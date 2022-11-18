29f736be0640a901d9e9d1e3d28e0ee505cea57f
awerni <andreas@wernitznig.com>
2022-11-18 23:58:24 +0100

    WorldCup 2022 update

--- Database/teams.sql
+++ Database/teams.sql
@@ -1,24 +1,39 @@
-INSERT INTO team(team, initialgroup) VALUES('Turkey', 'A');
-INSERT INTO team(team, initialgroup) VALUES('Italy', 'A');
-INSERT INTO team(team, initialgroup) VALUES('Wales', 'A');
-INSERT INTO team(team, initialgroup) VALUES('Switzerland', 'A');
-INSERT INTO team(team, initialgroup) VALUES('Denmark', 'B');
-INSERT INTO team(team, initialgroup) VALUES('Finland', 'B');
-INSERT INTO team(team, initialgroup) VALUES('Belgium', 'B');
-INSERT INTO team(team, initialgroup) VALUES('Russia', 'B');
-INSERT INTO team(team, initialgroup) VALUES('Netherlands', 'C');
-INSERT INTO team(team, initialgroup) VALUES('Ukraine', 'C');
-INSERT INTO team(team, initialgroup) VALUES('Austria', 'C');
-INSERT INTO team(team, initialgroup) VALUES('North Macedonia', 'C');
-INSERT INTO team(team, initialgroup) VALUES('England', 'D');
-INSERT INTO team(team, initialgroup) VALUES('Croatia', 'D');
-INSERT INTO team(team, initialgroup) VALUES('Scotland', 'D');
-INSERT INTO team(team, initialgroup) VALUES('Czech Republic', 'D');
+INSERT INTO team(team, initialgroup) VALUES('Qatar', 'A');
+INSERT INTO team(team, initialgroup) VALUES('Ecuador', 'A');
+INSERT INTO team(team, initialgroup) VALUES('Senegal', 'A');
+INSERT INTO team(team, initialgroup) VALUES('Netherlands', 'A');
+
+INSERT INTO team(team, initialgroup) VALUES('England', 'B');
+INSERT INTO team(team, initialgroup) VALUES('Iran', 'B');
+INSERT INTO team(team, initialgroup) VALUES('USA', 'B');
+INSERT INTO team(team, initialgroup) VALUES('Wales', 'B');
+
+INSERT INTO team(team, initialgroup) VALUES('Argentina', 'C');
+INSERT INTO team(team, initialgroup) VALUES('Saudi Arabia', 'C');
+INSERT INTO team(team, initialgroup) VALUES('Mexico', 'C');
+INSERT INTO team(team, initialgroup) VALUES('Poland', 'C');
+
+INSERT INTO team(team, initialgroup) VALUES('France', 'D');
+INSERT INTO team(team, initialgroup) VALUES('Australia', 'D');
+INSERT INTO team(team, initialgroup) VALUES('Denmark', 'D');
+INSERT INTO team(team, initialgroup) VALUES('Tunisia', 'D');
+
 INSERT INTO team(team, initialgroup) VALUES('Spain', 'E');
-INSERT INTO team(team, initialgroup) VALUES('Sweden', 'E');
-INSERT INTO team(team, initialgroup) VALUES('Poland', 'E');
-INSERT INTO team(team, initialgroup) VALUES('Slovakia', 'E');
-INSERT INTO team(team, initialgroup) VALUES('Hungary', 'F');
-INSERT INTO team(team, initialgroup) VALUES('Portugal', 'F');
-INSERT INTO team(team, initialgroup) VALUES('France', 'F');
-INSERT INTO team(team, initialgroup) VALUES('Germany', 'F');
+INSERT INTO team(team, initialgroup) VALUES('Costa Rica', 'E');
+INSERT INTO team(team, initialgroup) VALUES('Germany', 'E');
+INSERT INTO team(team, initialgroup) VALUES('Japan', 'E');
+
+INSERT INTO team(team, initialgroup) VALUES('Belgium', 'F');
+INSERT INTO team(team, initialgroup) VALUES('Canada', 'F');
+INSERT INTO team(team, initialgroup) VALUES('Morocco', 'F');
+INSERT INTO team(team, initialgroup) VALUES('Croatia', 'F');
+
+INSERT INTO team(team, initialgroup) VALUES('Brazil', 'G');
+INSERT INTO team(team, initialgroup) VALUES('Serbia', 'G');
+INSERT INTO team(team, initialgroup) VALUES('Switzerland', 'G');
+INSERT INTO team(team, initialgroup) VALUES('Cameroon', 'G');
+
+INSERT INTO team(team, initialgroup) VALUES('Portugal', 'H');
+INSERT INTO team(team, initialgroup) VALUES('Ghana', 'H');
+INSERT INTO team(team, initialgroup) VALUES('Uruguay', 'H');
+INSERT INTO team(team, initialgroup) VALUES('South Korea', 'H');
