f380c346431f4c63a3f9edf98e9f1f0eb8122d19
awerni <andreas@wernitznig.com>
2022-11-21 18:06:27 +0100

    added 3 games

--- Database/update.sql
+++ Database/update.sql
@@ -1,6 +1,6 @@
---update game set halftimegoals1 = 0, halftimegoals2 = 0, regulartimegoals1 = 0, regulartimegoals2 = 3 WHERE gameid = 1;
---update game set halftimegoals1 = 0, halftimegoals2 = 0, regulartimegoals1 = 1, regulartimegoals2 = 1 WHERE gameid = 2;
---update game set halftimegoals1 = 0, halftimegoals2 = 0, regulartimegoals1 = 0, regulartimegoals2 = 1 WHERE gameid = 3;
+update game set halftimegoals1 = 0, halftimegoals2 = 2, regulartimegoals1 = 0, regulartimegoals2 = 2 WHERE gameid = 1;
+update game set halftimegoals1 = 0, halftimegoals2 = 0, regulartimegoals1 = 0, regulartimegoals2 = 2 WHERE gameid = 2;
+update game set halftimegoals1 = 3, halftimegoals2 = 0, regulartimegoals1 = 6, regulartimegoals2 = 2 WHERE gameid = 3;
 --update game set halftimegoals1 = 2, halftimegoals2 = 0, regulartimegoals1 = 3, regulartimegoals2 = 0 WHERE gameid = 4;
 --update game set halftimegoals1 = 0, halftimegoals2 = 0, regulartimegoals1 = 3, regulartimegoals2 = 2 WHERE gameid = 5;
 --update game set halftimegoals1 = 1, halftimegoals2 = 1, regulartimegoals1 = 3, regulartimegoals2 = 1 WHERE gameid = 6;
