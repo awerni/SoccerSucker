8a62b9887658faf59ca6cd940ad0555fb94948e4
Andreas Wernitznig <awerni@users.noreply.github.com>
2024-06-04 15:40:20 +0200

    Delete Database/update.sh

--- Database/update.sh
+++
@@ -1,9 +0,0 @@
-#!/bin/sh
-
-cd /home/andreas/SoccerSucker/Database
-
-alias d='psql EuroCup2021 postgres -h localhost'
-
-echo "DROP TRIGGER checkTip ON tip;" | d 
-cat update.sql | d 
-echo "CREATE TRIGGER checkTip BEFORE INSERT OR UPDATE ON tip FOR EACH ROW EXECUTE PROCEDURE checkTip();" | d
