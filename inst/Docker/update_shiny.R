a819b0f74e66185fae0c4304333d5cc60d149264
awerni <andreas@wernitznig.com>
2024-06-07 22:56:09 +0200

    put both libraries into one function

--- inst/Docker/update_shiny.R
+++ inst/Docker/update_shiny.R
@@ -1,4 +1,4 @@
 install.packages(c(
-  "ellipsis" # shiny 1.7.5 prerequisite 
+  "ellipsis", # shiny 1.7.5 prerequisite
+  "https://cran.r-project.org/src/contrib/Archive/shiny/shiny_1.7.5.1.tar.gz"
 ))
-install.packages("https://cran.r-project.org/src/contrib/Archive/shiny/shiny_1.7.5.1.tar.gz")
