f466c5b82762dfacedd250e4b02f3dd5ad210865
awerni <andreas@wernitznig.com>
2024-06-07 22:56:57 +0200

    template for game results

--- Database/update.sql
+++ Database/update.sql
@@ -1,1 +1,1 @@
-UPDATE game set halftimegoals1 = 0, halftimegoals2 = 0, regulartimegoals1 = 0, regulartimegoals2 = 0 WHERE gameid = 1;
+UPDATE game set halftimegoals1 = 0, halftimegoals2 = 0, regulartimegoals1 = 1, regulartimegoals2 = 0 WHERE gameid = 0;
