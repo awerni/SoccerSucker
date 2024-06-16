c7c73769ddd739ddb550f364d03586de4282a334
awerni <andreas@wernitznig.com>
2024-06-16 17:44:08 +0200

    game update

--- Database/update.sql
+++ Database/update.sql
@@ -1,1 +1,6 @@
-UPDATE game set halftimegoals1 = 0, halftimegoals2 = 0, regulartimegoals1 = 1, regulartimegoals2 = 0 WHERE gameid = 0;
+UPDATE game set halftimegoals1 = 3, halftimegoals2 = 0, regulartimegoals1 = 5, regulartimegoals2 = 1 WHERE gameid = 1;
+UPDATE game set halftimegoals1 = 0, halftimegoals2 = 2, regulartimegoals1 = 1, regulartimegoals2 = 3 WHERE gameid = 2;
+UPDATE game set halftimegoals1 = 3, halftimegoals2 = 0, regulartimegoals1 = 3, regulartimegoals2 = 0 WHERE gameid = 3;
+UPDATE game set halftimegoals1 = 2, halftimegoals2 = 1, regulartimegoals1 = 2, regulartimegoals2 = 1 WHERE gameid = 4;
+
+UPDATE game set halftimegoals1 = 1, halftimegoals2 = 1, regulartimegoals1 = 1, regulartimegoals2 = 2 WHERE gameid = 7;
