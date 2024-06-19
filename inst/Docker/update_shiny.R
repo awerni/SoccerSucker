d265f30c2d2c0016f2b01959dc57b8bfd3c737e7
awerni <andreas@wernitznig.com>
2024-06-19 23:42:10 +0200

    split command into two

--- inst/Docker/update_shiny.R
+++ inst/Docker/update_shiny.R
@@ -1,4 +1,7 @@
-install.packages(c(
+install.packages(
   "ellipsis", # shiny 1.7.5 prerequisite
+  repos = "https://cran.wu.ac.at/"
+)
+install.packages(
   "https://cran.r-project.org/src/contrib/Archive/shiny/shiny_1.7.5.1.tar.gz"
-))
+)
