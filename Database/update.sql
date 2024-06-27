02ef14e32d3aee7436432474fef95b3890f25e83
awerni <andreas@wernitznig.com>
2024-06-27 08:18:14 +0200

    latest results

--- Database/update.sql
+++ Database/update.sql
@@ -26,6 +26,14 @@ UPDATE game set halftimegoals1 = 1, halftimegoals2 = 0, regulartimegoals1 = 1, r
 UPDATE game set halftimegoals1 = 0, halftimegoals2 = 0, regulartimegoals1 = 0, regulartimegoals2 = 1 WHERE gameid = 26 AND tournamentid = 1;
 UPDATE game set halftimegoals1 = 0, halftimegoals2 = 1, regulartimegoals1 = 0, regulartimegoals2 = 1 WHERE gameid = 27 AND tournamentid = 1;
 UPDATE game set halftimegoals1 = 0, halftimegoals2 = 0, regulartimegoals1 = 1, regulartimegoals2 = 1 WHERE gameid = 28 AND tournamentid = 1;
+UPDATE game set halftimegoals1 = 0, halftimegoals2 = 0, regulartimegoals1 = 0, regulartimegoals2 = 0 WHERE gameid = 29 AND tournamentid = 1;
+UPDATE game set halftimegoals1 = 0, halftimegoals2 = 0, regulartimegoals1 = 0, regulartimegoals2 = 0 WHERE gameid = 30 AND tournamentid = 1;
+UPDATE game set halftimegoals1 = 0, halftimegoals2 = 1, regulartimegoals1 = 2, regulartimegoals2 = 3 WHERE gameid = 31 AND tournamentid = 1;
+UPDATE game set halftimegoals1 = 0, halftimegoals2 = 0, regulartimegoals1 = 1, regulartimegoals2 = 1 WHERE gameid = 32 AND tournamentid = 1;
+UPDATE game set halftimegoals1 = 1, halftimegoals2 = 1, regulartimegoals1 = 1, regulartimegoals2 = 1 WHERE gameid = 33 AND tournamentid = 1;
+UPDATE game set halftimegoals1 = 0, halftimegoals2 = 0, regulartimegoals1 = 0, regulartimegoals2 = 0 WHERE gameid = 34 AND tournamentid = 1;
+UPDATE game set halftimegoals1 = 1, halftimegoals2 = 0, regulartimegoals1 = 2, regulartimegoals2 = 0 WHERE gameid = 35 AND tournamentid = 1;
+UPDATE game set halftimegoals1 = 0, halftimegoals2 = 0, regulartimegoals1 = 1, regulartimegoals2 = 2 WHERE gameid = 36 AND tournamentid = 1;
 
 
 UPDATE game set halftimegoals1 = 0, halftimegoals2 = 0, regulartimegoals1 = 2, regulartimegoals2 = 0 WHERE gameid = 1 AND tournamentid = 2;
@@ -36,3 +44,7 @@ UPDATE game set halftimegoals1 = 2, halftimegoals2 = 0, regulartimegoals1 = 2, r
 UPDATE game set halftimegoals1 = 1, halftimegoals2 = 0, regulartimegoals1 = 3, regulartimegoals2 = 1 WHERE gameid = 6 AND tournamentid = 2;
 UPDATE game set halftimegoals1 = 0, halftimegoals2 = 0, regulartimegoals1 = 0, regulartimegoals2 = 0 WHERE gameid = 7 AND tournamentid = 2;
 UPDATE game set halftimegoals1 = 2, halftimegoals2 = 0, regulartimegoals1 = 2, regulartimegoals2 = 1 WHERE gameid = 8 AND tournamentid = 2;
+UPDATE game set halftimegoals1 = 0, halftimegoals2 = 0, regulartimegoals1 = 0, regulartimegoals2 = 1 WHERE gameid = 9 AND tournamentid = 2;
+UPDATE game set halftimegoals1 = 0, halftimegoals2 = 0, regulartimegoals1 = 0, regulartimegoals2 = 1 WHERE gameid = 10 AND tournamentid = 2;
+UPDATE game set halftimegoals1 = 0, halftimegoals2 = 0, regulartimegoals1 = 1, regulartimegoals2 = 0 WHERE gameid = 11 AND tournamentid = 2;
+UPDATE game set halftimegoals1 = 2, halftimegoals2 = 0, regulartimegoals1 = 3, regulartimegoals2 = 1 WHERE gameid = 12 AND tournamentid = 2;
