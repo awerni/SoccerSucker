2f03d6cd37a88f40e29de7c745c951b979690dae
awerni <andreas@wernitznig.com>
2024-07-05 10:14:53 +0200

    latest copa game

--- Database/update.sql
+++ Database/update.sql
@@ -68,3 +68,5 @@ UPDATE game set halftimegoals1 = 0, halftimegoals2 = 0, regulartimegoals1 = 0, r
 UPDATE game set halftimegoals1 = 0, halftimegoals2 = 1, regulartimegoals1 = 1, regulartimegoals2 = 3 WHERE gameid = 22 AND tournamentid = 2;
 UPDATE game set halftimegoals1 = 1, halftimegoals2 = 1, regulartimegoals1 = 1, regulartimegoals2 = 1 WHERE gameid = 23 AND tournamentid = 2;
 UPDATE game set halftimegoals1 = 2, halftimegoals2 = 0, regulartimegoals1 = 2, regulartimegoals2 = 1 WHERE gameid = 24 AND tournamentid = 2;
+
+UPDATE game set halftimegoals1 = 1, halftimegoals2 = 0, regulartimegoals1 = 1, regulartimegoals2 = 1, overtimegoals1 = 1, overtimegoals2 = 1, penaltygoals1 = 4, penaltygoals2 = 2 WHERE gameid = 25 AND tournamentid = 2;
