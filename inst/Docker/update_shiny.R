ae8a2e57b1d5aaed1092d2a1bd48fc07202c400c
awerni <andreas@wernitznig.com>
2026-01-30 09:44:08 +0100

    removed

--- inst/Docker/update_shiny.R
+++
@@ -1,7 +0,0 @@
-install.packages(
-  "ellipsis", # shiny 1.7.5 prerequisite
-  repos = "https://cran.wu.ac.at/"
-)
-install.packages(
-  "https://cran.r-project.org/src/contrib/Archive/shiny/shiny_1.7.5.1.tar.gz"
-)
