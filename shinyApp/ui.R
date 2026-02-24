d49c60102647e5c26781b4407a589b4ad5537ff0
awerni <andreas@wernitznig.com>
2026-02-24 09:59:11 +0100

    new formatting

--- shinyApp/ui.R
+++ shinyApp/ui.R
@@ -11,65 +11,66 @@ fluidPage(theme = bs_theme(bootswatch = "cerulean"),
   tags$head(tags$script(src = "message-handler.js")),
   fluidRow(
     column(2,
-           img(src=logo_file, width="200", align = "center"),
-           hr(),
-           p("Version 1.5"),
-           hr(),
-           p(textOutput("timezone")),
-           hr(),
-           radioButtons("tournament", paste0(trans("select"), ":"), getTournament(), inline = FALSE),
-           hr(),
-           {
-             myChoise <- c("human", "human_bot", "bot")
-             names(myChoise) <- c(trans("human"), paste(trans("human"), "+", trans("bot")),  trans("bot"))
-             radioButtons("showplayers", paste0(trans("show"), ":"), myChoise)
-           },
-           hr(),
-           h3(textOutput("user")),
-           uiOutput("status"),
-           br(),
-           actionButton("refresh", label = trans("refresh"))
+      img(src = logo_file, width = "200", align = "center"),
+      hr(),
+      p("Version 1.5"),
+      hr(),
+      p(textOutput("timezone")),
+      actionButton("reload", label = "reload"),
+      hr(),
+      radioButtons("tournament", paste0(trans("select"), ":"), getTournament(), inline = FALSE),
+      hr(),
+      {
+        myChoise <- c("human", "human_bot", "bot")
+        names(myChoise) <- c(trans("human"), paste(trans("human"), "+", trans("bot")),  trans("bot"))
+        radioButtons("showplayers", paste0(trans("show"), ":"), myChoise)
+      },
+      hr(),
+      h3(textOutput("user")),
+      uiOutput("status"),
+      br(),
+      actionButton("refresh", label = trans("refresh"))
     ),
     column(10,
-           tabsetPanel(
-             tabPanel(trans("overallranking"), DT::dataTableOutput("ranking")),
-             tabPanel(trans("placebets"), uiOutput("placebets")),
-             tabPanel(trans("checkyourresults"), uiOutput("yourresults")),
-             tabPanel(trans("player_comparison"), tabsetPanel(
-               tabPanel(trans("heatmap"), plotOutput("heatmap", width = "100%", height = "650px")),
-               tabPanel(trans("lineranking"), uiOutput("rankingTab")),
-               tabPanel(trans("latestgames"), uiOutput("latestGames")),
-               tabPanel(trans("gamebet"),
-                 p(),
-                 selectInput("tipgame2show", "Select Game:", NULL),
-                 fluidRow(
-                   column(6, DT::dataTableOutput("gamebet")),  
-                   column(6, plotOutput("gamebetgraph", width = "100%", height = "500px"))
-                 )
-               ),
-               tabPanel(trans("pcapoints"), uiOutput("pcaPoints")),
-               tabPanel(trans("pcatips"), uiOutput("pcaTips")),
-               tabPanel(trans("missingbets"), DT::dataTableOutput("missingbets"))
-             )),
-             tabPanel(trans("summary_statistics"), tabsetPanel(
-               tabPanel(trans("nationality"), uiOutput("nationplot")),
-               tabPanel(trans("expertstatus"), uiOutput("expertplot")),
-               tabPanel(trans("pointsperteam"), plotOutput("pointsperteam", width = "100%", height = "400px"), textOutput("pointsperteamdesc"))
-             )),
-             tabPanel(trans("game_statistics"), tabsetPanel(
-               tabPanel(trans("gameresult"), p(), DT::dataTableOutput("gameresult")),
-               tabPanel(trans("teamranking"), p(), DT::dataTableOutput("teamranking"))
-             )),
-             tabPanel(
-               trans("help"), br(), trans("helptext"), hr(), br(),
-               p(
-                 a("registration help and legal disclaimer", href="register_help.html", target="_blank")
-               ),
-               p(
-                 R.Version()$version.string
-               )
-             )
-           )
+      tabsetPanel(
+        tabPanel(trans("overallranking"), DT::dataTableOutput("ranking")),
+        tabPanel(trans("placebets"), uiOutput("placebets")),
+        tabPanel(trans("checkyourresults"), uiOutput("yourresults")),
+        tabPanel(trans("player_comparison"), tabsetPanel(
+          tabPanel(trans("heatmap"), plotOutput("heatmap", width = "100%", height = "650px")),
+          tabPanel(trans("lineranking"), uiOutput("rankingTab")),
+          tabPanel(trans("latestgames"), uiOutput("latestGames")),
+          tabPanel(trans("gamebet"),
+            p(),
+            selectInput("tipgame2show", "Select Game:", NULL),
+            fluidRow(
+              column(6, DT::dataTableOutput("gamebet")),
+              column(6, plotOutput("gamebetgraph", width = "100%", height = "500px"))
+            )
+          ),
+          tabPanel(trans("pcapoints"), uiOutput("pcaPoints")),
+          tabPanel(trans("pcatips"), uiOutput("pcaTips")),
+          tabPanel(trans("missingbets"), DT::dataTableOutput("missingbets"))
+        )),
+        tabPanel(trans("summary_statistics"), tabsetPanel(
+          tabPanel(trans("nationality"), uiOutput("nationplot")),
+          tabPanel(trans("expertstatus"), uiOutput("expertplot")),
+          tabPanel(trans("pointsperteam"), plotOutput("pointsperteam", width = "100%", height = "400px"), textOutput("pointsperteamdesc"))
+        )),
+        tabPanel(trans("game_statistics"), tabsetPanel(
+          tabPanel(trans("gameresult"), p(), DT::dataTableOutput("gameresult")),
+          tabPanel(trans("teamranking"), p(), DT::dataTableOutput("teamranking"))
+        )),
+        tabPanel(
+          trans("help"), br(), trans("helptext"), hr(), br(),
+          p(
+            a("registration help and legal disclaimer", href="register_help.html", target="_blank")
+          ),
+          p(
+            R.Version()$version.string
+          )
+        )
+      )
     )
   )
 )
