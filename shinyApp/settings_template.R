5ba7daebec4c477aa3d6f66ed1cb72ed528ce66e
awerni <andreas@wernitznig.com>
2026-03-02 07:59:33 +0100

    time_zone settings

--- shinyApp/settings_template.R
+++ shinyApp/settings_template.R
@@ -1,10 +1,8 @@
 db <- list(dbname = "WorldCup2024", host = "myserver", port = 5432, user = "user", password = "pass")
+lang <- "en"
 
 logo_file <- "GoStrong.png"
-
-lang <- coalesce(Sys.getenv("lang", unset = NA), "de")
-#time_zone <- "America/Sao_Paulo"
-time_zone <- coalesce(Sys.getenv("timezone", unset = NA), "Europe/Paris")
+time_zone <- "Europe/Paris"
 
 checkAccount <- function(user, pass) {
   sql <- paste0("SELECT checklogin FROM checkLogin('", user, "', '", pass, "')")
