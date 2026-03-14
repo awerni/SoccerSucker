50080996d4e41c67c87dcfdf9322f445b67affa5
awerni <andreas@wernitznig.com>
2026-03-14 23:04:16 +0100

    added the group colors for team ranking

--- shinyApp/settings_template.R
+++ shinyApp/settings_template.R
@@ -8,3 +8,18 @@ checkAccount <- function(user, pass) {
   sql <- paste0("SELECT checklogin FROM checkLogin('", user, "', '", pass, "')")
   getPostgresql(sql)$checklogin > 0
 }
+
+group_colors <- c(
+  "#F5FFFA",  # Mint cream
+  "#FFFACD",  # Lemon chiffon
+  "#E6E6FA",  # Lavender
+  "#FAEBD7",  # Antique white
+  "#F0F8FF",  # Alice blue
+  "#E0FFFF",  # Light cyan
+  "#FFE4E1",  # Misty rose
+  "#F5F5DC",  # Beige
+  "#E0EEE0",  # Honeydew soft
+  "#FFF0F5",  # Lavender blush
+  "#F0FFF0",  # Honeydew
+  "#E6F2FF"   # Very light blue
+)
