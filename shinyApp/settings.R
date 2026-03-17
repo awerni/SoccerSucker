71c3eeb2159d2371f40f30b58d8abc3608c3d622
awerni <andreas@wernitznig.com>
2026-03-17 08:58:46 +0100

    replacement of settings_template.R

---
+++ shinyApp/settings.R
@@ -0,0 +1,34 @@
+db <- list(
+  dbname   = Sys.getenv("DB_NAME"),
+  host     = Sys.getenv("DB_HOST"),
+  port     = as.integer(Sys.getenv("DB_PORT")),
+  user     = Sys.getenv("DB_USER"),
+  password = Sys.getenv("DB_PASSWORD"),
+  sslmode  = Sys.getenv("DB_SSLMODE", unset = "require")
+)
+
+lang <- "de"
+
+logo_file <- "GoStrong.png"
+time_zone <- "Europe/Paris"
+#time_zone <- "America/Sao_Paulo"
+
+checkAccount <- function(user, pass) {
+  sql <- paste0("SELECT checklogin FROM checkLogin('", user, "', '", pass, "')")
+  getPostgresql(sql)$checklogin > 0
+}
+
+group_colors <- c(
+  "#F5FFFA",  # Mint cream
+  "#FFFACD",  # Lemon chiffon
+  "#E6E6FA",  # Lavender
+  "#FAEBD7",  # Antique white
+  "#F0F8FF",  # Alice blue
+  "#E0FFFF",  # Light cyan
+  "#FFE4E1",  # Misty rose
+  "#F5F5DC",  # Beige
+  "#E0EEE0",  # Honeydew soft
+  "#FFF0F5",  # Lavender blush
+  "#F0FFF0",  # Honeydew
+  "#E6F2FF"   # Very light blue
+)
