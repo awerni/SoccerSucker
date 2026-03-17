57f9005b93405303956494ad3b2b59f479cc2fd8
awerni <andreas@wernitznig.com>
2026-03-17 08:55:52 +0100

    added connection pool library

--- inst/Docker/install_prereqs.R
+++ inst/Docker/install_prereqs.R
@@ -2,7 +2,7 @@ install.packages(c(
   "remotes", # install helpers
   "purrr", "dplyr", "tidyr", "forcats", "ggplot2", "readr", "broom", # tidyverse (only required pieces)
   "shiny", "shinyjs", "shinyBS", "shinyWidgets", "shinythemes", "DT", # shiny-related
-  "DBI", "RPostgres", # DB handling
+  "DBI", "RPostgres", "pool", # DB handling
   "gggenes", "ggrepel", "plotROC", "pheatmap", "svglite", "gridExtra", # plotting
   "apcluster", "matrixTests", "ROCR", "Rtsne", "umap", "fpc", # cluster, dimred
   "logger", "config", "xfun", "xml2"
