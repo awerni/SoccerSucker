acd794fece02448e5fcbd6cfc9533f33ee08aff0
awerni <andreas@wernitznig.com>
2026-06-21 17:24:00 +0200

    initial import

---
+++ Database/fifasection.sql
@@ -0,0 +1,6 @@
+INSERT INTO fifasection(fifasection, continent) VALUES('AFC' ,'Asia');
+INSERT INTO fifasection(fifasection, continent) VALUES('CONCACAF', 'North, Central America and Caribbean');
+INSERT INTO fifasection(fifasection, continent) VALUES('UEFA', 'Europe');
+INSERT INTO fifasection(fifasection, continent) VALUES('CAF', 'Africa');
+INSERT INTO fifasection(fifasection, continent) VALUES('CONMEBOL', 'South America');
+INSERT INTO fifasection(fifasection, continent) VALUES('OFC', 'Oceania');
